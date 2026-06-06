fe00af3fb1216cb6b06541d640e62eff0b01e4a4
gdelacruz <gdelacruz@enaire.es>
2026-06-06 08:28:00 +0200

    [FEATURE] Añadir entidad JPA PriceEntity

---
+++ pricing-service/src/main/java/com/inditex/ecommerce/pricing/infrastructure/out/persistence/entity/PriceEntity.java
@@ -0,0 +1,45 @@
+package com.inditex.ecommerce.pricing.infrastructure.out.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "prices")
+@Getter
+@Setter
+public class PriceEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "brand_id")
+    private Long brandId;
+
+    @Column(name = "start_date")
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date")
+    private LocalDateTime endDate;
+
+    @Column(name = "price_list")
+    private Integer priceList;
+
+    @Column(name = "product_id")
+    private Long productId;
+
+    private Integer priority;
+
+    private BigDecimal price;
+
+    private String curr;
+}
